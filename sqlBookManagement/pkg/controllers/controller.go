cedbebffca9dd1d43a9c4fc865365d61b5761c4c
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2023-12-14 17:27:35 +0530

    MYSQL + GO Lang Book Management Server

--- sqlBookManagement/pkg/controllers/controller.go
+++ sqlBookManagement/pkg/controllers/controller.go
@@ -2,14 +2,24 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"sqlBookManagement/pkg/models"
+	"sqlBookManagement/pkg/utils"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
 
 func CreateBook(w http.ResponseWriter, req *http.Request) {
-
+	CreateBook := &models.Book{}
+	utils.ParseBody(req, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func GetBook(w http.ResponseWriter, req *http.Request) {
@@ -21,13 +31,55 @@ func GetBook(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetBookById(w http.ResponseWriter, req *http.Request) {
-
+	vars := mux.Vars(req)
+	bookId := vars["bookID"]
+	ID, err := strconv.ParseInt(bookId, 10, 32)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func UpdateBook(w http.ResponseWriter, req *http.Request) {
-
+	var updateBook = &models.Book{}
+	utils.ParseBody(req, updateBook)
+	vars := mux.Vars(req)
+	bookId := vars["bookID"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, db := models.GetBookById(ID)
+	if updateBook.Name != "" {
+		bookDetails.Name = updateBook.Name
+	}
+	if updateBook.Author != "" {
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != "" {
+		bookDetails.Publication = updateBook.Publication
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func DeleteBook(w http.ResponseWriter, req *http.Request) {
-
+	vars := mux.Vars(req)
+	bookId := vars["bookID"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
--- sqlBookManagement/pkg/utils/utils.go
+++ sqlBookManagement/pkg/utils/utils.go
@@ -2,14 +2,18 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
 
 func ParseBody(req *http.Request, x interface{}) {
 	if body, err := io.ReadAll(req.Body); err == nil {
+		fmt.Printf("%v\n", x)
 		if err := json.Unmarshal([]byte(body), x); err != nil {
+
 			return
 		}
+		fmt.Printf("%v", x)
 	}
 }
