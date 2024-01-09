03976542844a2e48a08c68a8b7c9c2c950e5caaf
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2024-01-09 17:35:09 +0530

    MYSQL + GO Lang Book Management Server

--- sqlBookManagement/go.mod
+++ sqlBookManagement/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	gorm.io/driver/mysql v1.5.2 // indirect
 	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55 // indirect
 )
--- sqlBookManagement/pkg/config/app.go
+++ sqlBookManagement/pkg/config/app.go
@@ -1,14 +1,18 @@
 package config
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var db *gorm.DB
 
 func Connect() {
-	d, err := gorm.Open("mysql", uri)
+	godotenv.Load()
+	d, err := gorm.Open("mysql", os.Getenv("URI"))
 	if err != nil {
 		panic(err)
 	}
